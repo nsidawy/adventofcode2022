02a36d80d7f59fed48dd67792cad3792b0b22310
Nick Sidawy <nick.sidawy@gmail.com>
2022-12-19 13:04:45 -0500

    day19p2 sort of

--- day19/Program.cs
+++ day19/Program.cs
@@ -5,7 +5,7 @@ internal class Program
     private static void Main(string[] args)
     {
         var robots = (1, 0, 0, 0);
-        var blueprints = File.ReadLines("test.txt")
+        var blueprints = File.ReadLines("input.txt")
             .Select(l => {
                 var values = Regex.Matches(l, @"\d+").Select(r => int.Parse(r.Value)).ToArray();
                 return new Blueprint {
@@ -20,17 +20,17 @@ internal class Program
             })
             .ToArray();
         
-        //var result = 0;
-        //foreach(var b in blueprints) {
-        //    result += Simulate(24, robots, (0, 0, 0, 0), b,
-        //        new Dictionary<(int, (int, int, int, int), (int, int, int, int)), int>()) * b.Id;
-        //    Console.WriteLine(result);
-        //}
-        //Console.WriteLine(result);
-        var result = 1;
+        var result = 0;
+        foreach(var b in blueprints) {
+            result += Simulate(24, robots, (0, 0, 0, 0), b,
+                new Dictionary<int, (int, int)>()) * b.Id;
+            Console.WriteLine(result);
+        }
+        Console.WriteLine(result);
+        result = 1;
         foreach(var b in blueprints.Take(3)) {
             result *= Simulate(32, robots, (0, 0, 0, 0), b,
-                new Dictionary<(int, (int, int, int, int), (int, int, int, int)), int>()) * b.Id;
+                new Dictionary<int, (int, int)>());
             Console.WriteLine(result);
         }
         Console.WriteLine(result);
@@ -41,21 +41,28 @@ internal class Program
             (int, int, int, int) robots,
             (int, int, int, int) resources,
             Blueprint blueprint,
-            Dictionary<(int, (int, int, int, int), (int, int, int, int)), int> memoize) 
+            Dictionary<int, (int, int)> memoize) 
         {
         if(time == 0) {
             return resources.Item4;
         }
-        if(memoize.ContainsKey((time, robots, resources))) {
-            return memoize[(time, robots, resources)];
+        var value = resources.Item4 * 1000000 + resources.Item3 * 10000 + resources.Item2 * 100 + resources.Item1;
+        if(memoize.ContainsKey(time)) {
+            var (bestValue, geodeCount) = memoize[time];
+            if (value / 1000000 < bestValue / 1000000) {
+                return geodeCount;
+            }
         }
         var canCreateOreRobot = resources.Item1 >= blueprint.OreRobotCost
-            && !(robots.Item1 >= blueprint.MaxOreCost);
+            && !(robots.Item1 >= blueprint.MaxOreCost)
+            && !(resources.Item1 > 20);
         var canCreateClayRobot = resources.Item1 >= blueprint.ClayRobotCost
-            && !(robots.Item2 >= blueprint.ObsidianRobotClayCost);
+            && !(robots.Item2 >= blueprint.ObsidianRobotClayCost)
+            && !(resources.Item2 > 20);
         var canCreateObsidianRobot = resources.Item1 >= blueprint.ObsidianRobotOreCost
             && resources.Item2 >= blueprint.ObsidianRobotClayCost
-            && !(robots.Item3 >= blueprint.GeodeRobotObsidianCost);
+            && !(robots.Item3 >= blueprint.GeodeRobotObsidianCost)
+            && !(resources.Item3 > 20);
         var canCreateGeodeRobot = resources.Item1 >= blueprint.GeodeRobotOreCost
             && resources.Item3 >= blueprint.GeodeRobotObsidianCost;
         resources.Item1 += robots.Item1;
@@ -64,6 +71,12 @@ internal class Program
         resources.Item4 += robots.Item4;
 
         var results = new List<int>();
+        if(!(robots.Item2 == 0 && canCreateOreRobot && canCreateClayRobot)
+            && !(robots.Item3 == 0 && canCreateOreRobot && canCreateClayRobot && canCreateObsidianRobot)
+            && !(canCreateOreRobot && canCreateClayRobot && canCreateObsidianRobot && canCreateGeodeRobot))
+        {
+            results.Add(Simulate(time - 1, robots, resources, blueprint, memoize));
+        }
         if(canCreateGeodeRobot) {
             results.Add(Simulate(
                 time -1 ,
@@ -92,8 +105,7 @@ internal class Program
                 (resources.Item1 - blueprint.OreRobotCost, resources.Item2, resources.Item3, resources.Item4),
                 blueprint, memoize));
         }
-        results.Add(Simulate(time - 1, robots, resources, blueprint, memoize));
-        memoize[(time, robots, resources)] = results.Max();
+        memoize[time] = (value, results.Max());
         return results.Max();
     }
 
